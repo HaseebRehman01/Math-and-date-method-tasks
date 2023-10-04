bd11958cc3f6285362cc34ef25a19e83ce650a66
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-04 19:19:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // task 1
+var input = prompt("Write num")
+console.log(input)
+console.log("round" + ":"+Math.round(input)  )
+console.log("ceil" + ":"+Math.ceil(input)  )
+console.log("floor" + ":"+Math.floor(input)  )

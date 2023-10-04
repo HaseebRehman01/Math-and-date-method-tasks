9f53441bf7c43b4d33d3f9f2c04463056bba0e95
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-04 19:32:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -25,16 +25,20 @@
 
 
 // }
-var input = prompt("Write num")
+// task 4
+// var input = prompt("Write num")
 
-if (!isNaN(input)){
-console.log(input)
-console.log("round" + ":"+Math.round(input)  )
-console.log("ceil" + ":"+Math.ceil(input)  )
-console.log("floor" + ":"+Math.floor(input)  )
-}
-else{
-    console.log("Write valid input")
+// if (!isNaN(input)){
+// console.log(input)
+// console.log("round" + ":"+Math.round(input)  )
+// console.log("ceil" + ":"+Math.ceil(input)  )
+// console.log("floor" + ":"+Math.floor(input)  )
+// }
+// else{
+//     console.log("Write valid input")
 
 
-}
+// }
+
+
+// task 5
